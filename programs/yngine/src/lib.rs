87c1354124fc5339fa5eb3651d53c93a3b144ea9
Uday Kumar <kuday0726@gmail.com>
2025-10-29 23:22:40 +0000

    feat(vault): add Vault state structure and module setup for initialization

--- programs/yngine/src/lib.rs
+++ programs/yngine/src/lib.rs
@@ -1,5 +1,9 @@
+#![allow(unexpected_cfgs)]
+#![allow(deprecated)]
 use anchor_lang::prelude::*;
+use crate::instructions::*;
 
+pub mod instructions;
 pub mod state;
 
 declare_id!("3kLpfgTzRrMDANpQQ9gFnENqvFFQGyU7apooz426YWty");
@@ -7,7 +11,6 @@ declare_id!("3kLpfgTzRrMDANpQQ9gFnENqvFFQGyU7apooz426YWty");
 #[program]
 pub mod yngine {
     use super::*;
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
--- programs/yngine/src/state.rs
+++ programs/yngine/src/state.rs
@@ -1,17 +1,25 @@
 use anchor_lang::prelude::*;
-#[account]
 
+#[account]
 pub struct Vault {
     pub owner: Pubkey,
     pub ynsol_mint: Pubkey, 
     pub sol_balance: u64,
+    pub active_provider:u16,
+    pub created_at: u8,
     pub bump: u8,
-    pub active_provider: u16,
 }
+
+impl Vault {
+    pub const SIZE: usize = 8+32+32+4+2+1+1;
+}
+
+#[account]
 pub struct User {
     pub owner: Pubkey,
     pub vault: Pubkey, //user's vault address
     pub deposited_sol:Pubkey,
     pub withdrawn_sol: Pubkey,
+    pub created_at: u8,
     pub bump: u8,
 }
