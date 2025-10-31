99f876356be1fb7a2bc26ae615af4ba45dad72ba
Uday Kumar <kuday0726@gmail.com>
2025-10-31 08:16:56 +0000

    feat(struct): add deposit structs

--- programs/yngine/src/instructions/deposit.rs
+++ programs/yngine/src/instructions/deposit.rs
@@ -1,7 +1,11 @@
 #![allow(deprecated)]
 #![allow(unexpected_cfgs)]
 
+use std::task::Context;
+
 use anchor_lang::prelude::*;
+use anchor_spl::token::{TokenAccount,Mint};
+use anchor_spl::associated_token::AssocaitedToken;
 use crate::state::{Vault,User};
 
 #[derive(Accounts)]
@@ -25,6 +29,25 @@ pub struct Deposit<'info>{
     //user token account for wsol,jitosol etc
     #[account(mut)]
     pub user_token_account: Account<'info,TokenAccount>,
+    //vault token account for recieving user's funds
+    pub vault_token_account: Account<'info,TokenAccount>,
+    //ynSOL which is minted to User
+    #[account(mut)]
+    pub ynsol_mint: Account<'info,Mint>,
+    //ata account user recieves ynsol
+    pub user_ynsol_ata: Account<'info,TokenAccount>,
+    //token program
+    pub token_program: Program<'info,Token>,
+
+    pub system_program: Program<'info,System>,
+    pub associated_token_program: Program<'info,AssocaitedToken>,
+    pub rent: Sysvar<'info,Rent>
+}
+pub fn deposit(
+    ctx:Context<Deposit>,
+
+)->Result<()>{
+
 }
 
 
